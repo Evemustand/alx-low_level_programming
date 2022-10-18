a88da99ca71a3e3a9413cfc689320093d6d284dd
Evemustand <evalynemurugami@gmail.com>
2022-10-18 11:47:59 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char lett;
+
+	while (count++ <= 9)
+	{
+	for (lett = 'a'; lett <= 'z'; lett++)
+		_putchar(lett);
+
+	_putchar('\n');
+	}
+}
