741f145f02e6913dab01cb543fad145c72268567
Evemustand <evalynemurugami@gmail.com>
2022-10-18 16:14:11 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 /**
- * main - size of array
- * Return: Always 0
- */
+ * main - Entry
+ * Return: 0
+*/
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a char: %zu byte(S)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %zu byte(S)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %zu byte(S)\n", (unsigned long)sizeof(c));
-	printf("Size of long long int: %zu byte(S)\n", (unsigned long)sizeof(d));
-	printf("Size of float: %zu byte(S)\n", (unsigned long)sizeof(e));
+	printf("size of a char: %d byte(S)\n", sizeof(char));
+	printf("size of an int: %d byte(S)\n", sizeof(int));
+	printf("size of a long int: %d byte(S)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(S)\n", sizeof(long long int));
+	printf("size of a float: %d byte(S)\n", sizeof(float));
 	return (0);
 }
