25d4cdd95409fa089aed2649050e3c01d8510acb
Evemustand <evalynemurugami@gmail.com>
2022-10-28 03:27:17 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates two strings
+* @dest: pointer to the string to be concatenate
+* @src: source str to be appended
+* Return: pointer to the destination string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+		while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] - src[index];
+
+	return (dest);
+}
