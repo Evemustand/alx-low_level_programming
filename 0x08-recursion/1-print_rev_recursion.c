1561fa095ad763861f3250d50b5fd7598364fa7e
Evemustand <evalynemurugami@gmail.com>
2022-11-03 08:29:16 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,10 +7,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar(*s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
