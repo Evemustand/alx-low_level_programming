14bb8beb5cf96c322489e1a931d7a588ba100521
Evemustand <evalynemurugami@gmail.com>
2022-11-08 09:33:31 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - it print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
