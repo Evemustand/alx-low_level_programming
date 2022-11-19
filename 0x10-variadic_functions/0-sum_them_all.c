677a20a3bebffbd617160ab00b77a1c944b893e0
Evemustand <evalynemurugami@gmail.com>
2022-11-19 10:32:57 -0800

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -24,8 +24,9 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (index = 0; index < n; index++)
 
-		sum += va_arg(nums, int);
+		sum = va_arg(nums, int);
 
 	va_end(nums);
+
 	return (sum);
 }
